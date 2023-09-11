f118fcaaf1ab33e7d44ae59b669ff3bc7f3c7811
Camilo <cparada2022@uct.alu.cl>
2023-09-11 14:57:02 -0300

    Estilización de Login y Register

--- src/Components/LoginRegister/LoginRegister.jsx
+++ src/Components/LoginRegister/LoginRegister.jsx
@@ -3,19 +3,24 @@ import './LoginRegister.css'
 import user_icon from '../Assets/usuario.png'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/contraseña.png'
+import logo from '../Assets/Logo.png'
 
 export const LoginRegister = () => {
 
     const[action,setAction] = useState("Registrar");
 
   return (
+    <div className="outer-container">
+        <div className="logo-container">
+            <img src={logo} alt="" className='logo'/>
+        </div>
     <div className='container'>
         <div className='header'>
             <div className="text">{action}</div>
             <div className="underline"></div>
         </div>
         <div className="inputs">
-            {action==="Conectar"?<div></div>:<div className="input">
+            {action==="Iniciar Sesión"?<div></div>:<div className="input">
                 <img src={user_icon} alt="" />
                 <input type="text" placeholder='Nombre'/>
             </div>}
@@ -26,14 +31,15 @@ export const LoginRegister = () => {
             </div>
             <div className="input">
                 <img src={password_icon} alt="" />
-                <input type="password" placeholder='Tu contraseña'/>
+                <input type="password" placeholder='Contraseña'/>
             </div>
-            {action==="Registrar"?<div></div>:<div className="forgot-password">¿Perdiste tu contraseña? <span>Presiona aquí</span></div>}
+            {action==="Registrar"?<div></div>:<div className="forgot-password"><strong>¿Perdiste tu contraseña?</strong> <span>Presiona aquí</span></div>}
             <div className="submit-container">
-                <div className={action==="Conectar"?"submit gray":"submit"} onClick={()=>{setAction("Registrar")}}>Registrar</div>
-                <div className={action==="Registrar"?"submit gray":"submit"}onClick={()=>{setAction("Conectar")}}>Conectar</div>
+                <div className={action==="Iniciar Sesión"?"submit active":"submit"} onClick={()=>{setAction("Registrar")}}>Registrar</div>
+                <div className={action==="Registrar"?"submit active":"submit"}onClick={()=>{setAction("Iniciar Sesión")}}>Iniciar Sesión</div>
             </div>
         </div>
+    </div>
     </div>
   )
 }
