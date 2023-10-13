4a9a23f5381230a25d751fab2552fcb65821ee9d
Blackcito <137465887+Blackcito@users.noreply.github.com>
2023-10-13 18:34:44 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,79 +1,65 @@
 const express = require('express');
-const app = express();
-const db = require('./db.js'); // 
 const cors = require('cors');
-const bcrypt = require('bcrypt'); // Libreria que se usara para encriptar la contraseña
-// comando para instalar: npm install bcrypt
+const app = express();
+const { Client } = require('pg');
 
 
-app.use(cors({ origin: 'http://localhost:5173' }));
-app.use(express.json());
+// Habilitar CORS para todas las rutas
+app.use(cors());
 
-app.post('/registro', async (req, res) => {
-  const { nombre, correo, contrasena } = req.body;
+//BASE DE DATOS
 
+const client = process.env.CONNECTION_STRING
+
+client.connect()
+  .then(() => {
+    console.log('Conexión exitosa a la base de datos');
+  })
+  .catch((error) => {
+    console.error('Error de conexión a la base de datos:', error);
+  })
+
+
+
+app.get('/api/ventas-por-dia', async (req, res) => {
   try {
-
-// Genera un salt (En este caso seran 10 rondas de hashing para asegurar la contraseña)
-    const saltRounds = 10;
-
-// Se genera el hash de la contraseña, la cual toma como argumentos la contreña y el valor de rondas de hashing
-
-    const hashedPassword = await bcrypt.hash(contrasena, saltRounds);
-
-// Consulta para ver si el correo o el nombre del usuario existen en la base de datos
-
-    const usuarioExistente = await db.oneOrNone('SELECT * FROM registro WHERE correo = $1 OR nombre = $2', [
-      correo,
-      nombre,
-    ]);
-
-// Si el usuario ya existe, enviar un mensaje de error 
-
-    if (usuarioExistente) {
-      console.log('El correo o el nombre de usuario que esta colocando en el campo ya están registrados.');
-      return res.status(400).send('El correo o el nombre de usuario ya están registrados.');
-    }
-
-// Si el usuario no existe se guardaran sus datos en la base de datos
-    await db.none('INSERT INTO registro(nombre, correo, contrasena) VALUES ($1, $2, $3)', [
-      nombre,
-      correo,
-      hashedPassword,
-    ]);
-
-// Y para finalizar se enviara un mensaje en caso de que el registro haya sido exitoso
-    res.status(200).send('¡Formulario exitoso!');
-    console.log('registro exitoso');
-
-  } 
-    catch (error) {
-    console.error('Error al insertar en la base de datos: ', error);
-    res.status(500).send('Error al procesar el formulario');
+    const result = await client.query(`
+      SELECT DiaSemana, COUNT(*) as CantidadVentas
+    FROM Ventas
+    GROUP BY DiaSemana
+    ORDER BY
+      CASE
+        WHEN DiaSemana = 'lunes' THEN 1
+        WHEN DiaSemana = 'martes' THEN 2
+        WHEN DiaSemana = 'miércoles' THEN 3
+        WHEN DiaSemana = 'jueves' THEN 4
+        WHEN DiaSemana = 'viernes' THEN 5
+        WHEN DiaSemana = 'sábado' THEN 6
+        WHEN DiaSemana = 'domingo' THEN 7
+        ELSE 8 -- Para cualquier otro valor
+      END;
+    `)
+    const rows = result.rows;
+    res.json(rows);
+  } catch (error) {
+    console.error('Error al obtener datos de ventas:', error);
+    res.status(500).json({ error: 'Error al obtener datos de ventas' });
   }
 });
 
 
+// Ruta que envía datos al cliente
+app.get('/api/data', (req, res) => {
+  const data = {
+    message: 'Hola desde Node.js',
+    // Otros datos que desees enviar
+  };
+  res.json(data);
+});
+
+// Resto de la configuración de Express
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor funcionando en el puerto ${port}`);
 });
-
-
-
-
-
-//  Especifico la consulta SQL
-//const sqlQuery = 'SELECT * FROM clientes'
-
-//  Uso la consulta a la base de datos
-//db.query(sqlQuery)
-  //.then(data => {
-  // Maneja los resultados aquí
-    //console.log('Resultados de la consulta:', data)
-  //})
-  //.catch(error => {
-  // Maneja los errores aquí
-    //console.error('Error al ejecutar la consulta:', error)
-  //})
