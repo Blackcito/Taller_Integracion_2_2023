bf55cba86653a2674622646b776a40597e9fa004
Jose Miguel Alderete <Jalderetelopez@gmail.com>
2023-11-19 20:46:27 -0300

    a
    a

--- backend/controller/register.js
+++ backend/controller/register.js
@@ -1,10 +1,11 @@
-const { db } = require('../db/db'); // Importa la instancia de cliente de db.js
+const  db  = require('../db/db'); // Importa la instancia de cliente de db.js
 const bcrypt = require('bcrypt'); // Libreria que se usara para encriptar la contraseña
-const registerUser = async (user, email, pass) => {
+const jwt = require('jsonwebtoken');
+
+const registerUser = async (user, email, pass,type) => {
   try {
-   
     // Consulta para ver si el correo o el nombre del usuario existen en la base de datos
-    const usuarioExistente = await db.oneOrNone('SELECT * FROM clientes WHERE email = $1 OR id_usuario = $2', [
+    const usuarioExistente = await db.oneOrNone('SELECT * FROM registro WHERE email = $1 OR id_usuario = $2', [
       email,
       user,
     ]);
@@ -23,17 +24,26 @@ const registerUser = async (user, email, pass) => {
     // Se genera el hash de la contraseña, la cual toma como argumentos la contreña y el valor de rondas de hashing
     const hashedPassword = await bcrypt.hash(pass, saltRounds);
 
+    // Valor para la columna administrador
+    const admin = false;
+    const role = 'user';  // Se puede asignar 'admin' si es un administrador
     // Parámetros de la consulta
-    const values = [user, email, hashedPassword];
+
+    const token = jwt.sign({ id_usuario: user, admin: admin }, 'Secreto_XD'); // 'Secreto_XD' es una clave secreta, asegúrate de guardarla de forma segura
+
+    const values = [user, email, hashedPassword, token, admin, role ,type];
 
     const query = `
-    INSERT INTO "clientes" ("id_usuario", "email", "password")
-    VALUES ($1, $2, $3)
+    INSERT INTO "registro" ("id_usuario", "email", "password", "token",  "admin", "role")
+    VALUES ($1, $2, $3, $4, $5, $6)
   `;
-
 
     // Ejecuta la consulta SQL con los valores proporcionados
     await db.none(query, values);
+
+
+
+    return token
   } catch (error) {
     throw error;
   }
--- backend/main.js
+++ backend/main.js
@@ -4,29 +4,35 @@ const cors = require('cors');
 const { registerUser } = require('./controller/register')
 const { loginUser } = require('./controller/login');
 const { getVentasPorDiaQuery } = require('./query/queries_ventas'); 
-const { db } = require('./db/db'); // Importa la instancia de cliente de db.js
-
-
+const {VerificarToken} = require("./middleware/verifyToken");
+const { updateUserProfile}=require('./controller/updateprofile')
+const db = require('./db/db')
+const catalogoController = require('./controller/catalogo')
 
 // comando para instalar: npm install bcrypt
 app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
+app.use('/api', catalogoController)
+//app.use('/api', sushiBuildController)
 
-//REGISTRO Y LOGIN
-
-app.post('/registro', async (req, res) => {
+app.post('/register', async (req, res) => {
   try {
-    const { user, email, pass } = req.body;
+    const { user, email, pass,type } = req.body;
 
     // Verifica si los datos requeridos existen en la solicitud
-    if (!user || !email || !pass) {
+    if (!user || !email || !pass ) {
       return res.status(400).send('Faltan datos requeridos.');
     }
 
-    await registerUser(user, email, pass);
-    res.status(201).send('Usuario registrado correctamente');
+    const token = await registerUser(user, email, pass,type); // Captura el token generado
+    // constante token que servira para contener la información del usuario identificado
+    
+
+    res.status(201).json({ message: 'Usuario registrado correctamente', token });
+
     console.log('Usuario registrado exitosamente');
+
   } catch (error) {
     console.error('Error al registrar al usuario:', error);
 
@@ -42,23 +48,44 @@ app.post('/registro', async (req, res) => {
 
 
 app.post('/login', async (req, res) => {
-  const { email, pass } = req.body;
+  const { email, pass, type } = req.body;
 
   try {
-    const usuario = await loginUser(email, pass);
+    const { userData, token } = await loginUser(email, pass, type);
 
-    if (usuario) {
       console.log('Su Inicio de sesión ha sido exitoso');
-      res.status(200).send('Su Inicio de sesión ha sido exitoso');
-    } else {
-      console.log('Correo o contraseña incorrectos.');
-      res.status(400).send('Correo o contraseña incorrectos.');
-    }
+      res.status(200).send({ message: 'Su Inicio de sesión ha sido exitoso', token, usuario: userData });
+
   } catch (error) {
-    console.error('Error al autenticar al usuario:', error);
-    res.status(500).send('Error al procesar la solicitud');
+
+    if (error.statusCode === 400) {
+      // Error específico: contraseña incorrecta o usuario no encontrado
+      res.status(400).send(error.message);
+    } else {
+      // Otros errores
+      res.status(500).send('Error al procesar la solicitud');
+    }
   }
 });
+
+
+
+app.get('/api/data', VerificarToken, (req, res) => {
+  // El middleware de verificación del token ya ha ejecutado, por lo que req.user contiene la información del usuario
+  if (req.user.role === 'admin') {
+    // Usuario es un administrador, permitir el acceso
+    const data = {
+      message: 'Hola desde Node.js (Acceso para administradores)',
+      // Otros datos que desees enviar
+    };
+    res.json(data);
+  } else {
+    // Usuario no es un administrador, devolver un mensaje de acceso denegado
+    res.status(403).json({ message: 'Acceso denegado para usuarios no administradores' });
+  }
+});
+
+
 
 
 app.get('/api/ventas-por-dia', async (req, res) => {
@@ -82,44 +109,73 @@ app.get('/api/ventas-por-dia', async (req, res) => {
 
 
 
+app.get('/datos-usuario', VerificarToken,  (req,res) => {  // primero que todo se define la ruta "get" para solicitar los datos
+  // lo cual antes se ejecutara primero la funcion "VerificarToken" antes de que llegue a la funcion de manejo de la ruta
+  
+  
+    if (!req.user || !req.user.id_usuario || !req.user.email) { // se asigna una condicion, la cual es que si no existe "req.user"
+    //  (informacion decodificada del usuario) o no existe "id_usuario" o "email", arrojara el error (en este caso '401')
+      return res.status(401).json({ message: 'Acceso no autorizado' });
+    }
+  
+  //  se crea el objeto "usuario" el cual tendra las propiedades "id_usuario" y "email" del objeto "req.user"
+    const usuario= {
+      id_usuario: req.user.id_usuario,
+      email : req.user.email,
+    };
+  
+    res.status(200).json({usuario});
+  });
 
+// ...
 
+app.post('/actualizar-datos', VerificarToken, async (req, res) => {
+  try {
+    const { id_usuario, email } = req.body;
 
+    // Verifica si los datos requeridos existen en la solicitud
+    if (!id_usuario || !email) {
+      return res.status(400).send('Faltan datos requeridos para la actualización.');
+    }
 
+    // Llama a la función de controlador para actualizar el perfil
+    await updateUserProfile(id_usuario, email);
 
-app.get('/api/data', (req, res) => {
-  const data = {
-    message: 'Hola desde Node.js',
-    // Otros datos que desees enviar
-  };
-  res.json(data);
+    res.status(200).json({ message: 'Datos de usuario actualizados correctamente' });
+
+  } catch (error) {
+    console.error('Error al actualizar datos de usuario:', error);
+    res.status(500).json({ error: 'Error interno del servidor al actualizar datos de usuario' });
+  }
 });
 
 
-//Graficas
+
+app.post('/api/agregar-al-carrito', async (req, res) => {
+  try {
+    const { id_usuario, id_producto, cantidad } = req.body;
+
+    // Aquí ejecuta la consulta SQL para insertar el producto en la tabla del carrito
+    // Utiliza el id_usuario y id_producto recibidos para realizar la inserción
+
+    // Ejemplo de consulta (asegúrate de usar tu propio método para interactuar con la base de datos)
+    const query = `INSERT INTO carrito_compras (id_usuario, id_producto, cantidad) VALUES ($1, $2, $3)`;
+    await db.none(query, [id_usuario, id_producto, cantidad]);
+
+    res.status(200).json({ message: 'Producto agregado al carrito correctamente' });
+    console.log('Producto agregado al carrito correctamente');
+  } catch (error) {
+    console.error('Error al agregar producto al carrito:', error);
+    res.status(500).json({ error: 'Error interno del servidor al agregar producto al carrito' });
+  }
+});
 
 
+
+  
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor funcionando en el puerto ${port}`);
 });
-
-
-
-
-
-//  Especifico la consulta SQL
-//const sqlQuery = 'SELECT * FROM clientes'
-
-//  Uso la consulta a la base de datos
-//db.query(sqlQuery)
-  //.then(data => {
-  // Maneja los resultados aquí
-    //console.log('Resultados de la consulta:', data)
-  //})
-  //.catch(error => {
-  // Maneja los errores aquí
-    //console.error('Error al ejecutar la consulta:', error)
-  //})
